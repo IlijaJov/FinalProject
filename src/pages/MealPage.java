540c527610e4aa08aa3b2d371f78813fd4473a24
IlijaJov <ijovanovic394@gmail.com>
2021-06-21 19:42:19 +0200

    Created MealPage

---
+++ src/pages/MealPage.java
@@ -0,0 +1,48 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MealPage extends BasicPage {
+
+	public MealPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js, Actions actions) {
+		super(driver, wait, js, actions);
+	}
+	
+//	get metode za sve potrebne elemente
+
+	public WebElement mealsBtn() {
+		return driver.findElement(By.xpath("//*[contains(text(),'Meals')]"));
+	}
+	
+	public WebElement addToCart() {
+		return driver.findElement(By.xpath("//*[contains(text(),'Add To Cart')]"));
+	}
+	
+	public WebElement quantityBtn() {
+		return driver.findElement(By.name("product_qty"));
+	}
+	
+//	metodu koja jelo dodaje u omiljena jela, klikom na dugme Favorite 
+	public void addToFavorite() {
+		driver.findElement(By.xpath("//a[@title='Favorite']")).click();
+	}
+//	metodu koja dodaje jelo u korpu - kao parametar se prosleđuje količina
+	public void addMeal(String meal, int quantity) throws InterruptedException {
+		this.mealsBtn().click();
+		Thread.sleep(2000);
+		driver.navigate().to(meal);
+		Thread.sleep(2000);
+		this.quantityBtn().sendKeys(Keys.BACK_SPACE);
+		Thread.sleep(500);
+		this.quantityBtn().sendKeys(String.valueOf(quantity));
+		Thread.sleep(500);
+		this.addToCart().click();
+	}
+
+}
