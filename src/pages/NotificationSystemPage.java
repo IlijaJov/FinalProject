8199a280d5b6945dcee3a0c156a2956746d04049
IlijaJov <ijovanovic394@gmail.com>
2021-06-21 19:44:45 +0200

    Modified NotificationSystemPage

--- src/pages/NotificationSystemPage.java
+++ src/pages/NotificationSystemPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,8 +13,8 @@ public class NotificationSystemPage extends BasicPage{
 
 //	Notification Sistem Page:
 
-	public NotificationSystemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
-		super(driver, wait, js);
+	public NotificationSystemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js, Actions actions) {
+		super(driver, wait, js, actions);
 		this.wait = new WebDriverWait(driver, 30, 10);
 	}
 	
