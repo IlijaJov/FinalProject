d607810a3310008cc86ff155860e69bff770f89b
IlijaJov <ijovanovic394@gmail.com>
2021-06-21 19:43:22 +0200

    Modified BasicPage

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasicPage {
@@ -10,11 +11,13 @@ public abstract class BasicPage {
 	protected WebDriver driver;
 	protected WebDriverWait wait;
 	protected JavascriptExecutor js;
+	protected Actions actions;
 	
-	public BasicPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js ) {
+	public BasicPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js, Actions actions ) {
 		this.driver = driver;
 		this.wait = wait;
 		this.js = js; 
+		this.actions = actions;
 	}
 	
 	public boolean elementExists(By by) {
